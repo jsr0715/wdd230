ecc89beb8c6f45ce4670ff81516768cdce9f71c0
jsr0715 <surim0715@gmail.com>
2022-04-27 23:04:03 -0600

    changes

---
+++ lesson2/script.js
@@ -0,0 +1,6 @@
+let d = new Date();
+
+document.getElementById("currentYear").textContent = d.getFullYear();
+
+let currentdate = document. lastModified;
+                    document.getElementById('modified').textContent = 'Last Updated:' + currentdate;
