6793b5bc296e74d86faffc1166e2de162193b14f
jsr0715 <surim0715@gmail.com>
2022-05-05 17:13:21 -0600

    change

---
+++ lesson2-2/scripts/lastUpdated.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {        
+    // get and format current date 
+    let getDate = new Date();
+    let dayOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric' 
+    };
+    let todaysDate = getDate.toLocaleDateString(undefined, dayOptions);
+
+    // update the last modified date and time
+    document.querySelector('#last-updated').textContent = 'Last Modified: ' + todaysDate;   
+ }, false); 
--- lesson3-2/scripts/date.js
+++
@@ -1,6 +0,0 @@
-let d = new Date();
-
-document.getElementById("currentYear").textContent = d.getFullYear();
-
-let currentdate = document. lastModified;
-                    document.getElementById('modified').textContent = 'Last Updated:' + currentdate;
